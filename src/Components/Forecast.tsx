78684247ca71a62454539f2694ab1e5840f16904
Emir Mutlu <emirmutlu169@icloud.com>
2024-06-05 17:01:35 +0300

    api edited

--- src/Components/Forecast.tsx
+++ src/Components/Forecast.tsx
@@ -12,7 +12,7 @@ const Forecast: React.FC<any> = () => {
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((success) => {
-            axios.get('/forecast', {
+            axios.get('https://api.open-meteo.com/v1/forecast', {
                 params: {
                     lat: success.coords.latitude,
                     lon: success.coords.longitude,
